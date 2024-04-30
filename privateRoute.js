bc1aea9d7c485b9674472fe36b5de7a11a080fdf
Mark Jaspher Ramile <mjaspher.ramile@gmail.com>
2024-04-30 18:09:09 +0800

    added sessionStorage watcher

--- privateRoute.js
+++ privateRoute.js
@@ -3,10 +3,26 @@ import { useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
   const location = useLocation();
+  const [isSessionActive, setSessionActive] = useState(isAuthenticated);
 
-  if (!isAuthenticated) {
+  const handleStorageChange = useCallback((event) => {
+    if (event.storageArea === sessionStorage && sessionStorage.length === 0) {
+      setSessionActive(false);
+      dispatch(logoutUser());
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    window.addEventListener('storage', handleStorageChange);
+    return () => {
+      window.removeEventListener('storage', handleStorageChange);
+    };
+  }, [handleStorageChange]);
+
+  if (!isSessionActive) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
